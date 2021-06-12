d92e5c17fc1a722e2b7eb71c744a5ed7ab213ed0
sagsaguz <sagsaguz@gmail.com>
2021-06-12 15:32:39 +0530

    Added dependencies for Location. Getting current location details.

---
+++ app/src/main/java/sagar/wellthytest/WeatherReportActivity.kt
@@ -0,0 +1,120 @@
+package sagar.wellthytest
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.model.LatLng
+import sagar.wellthytest.utils.PermissionUtils.LOCATION_PERMISSION_REQUEST_CODE
+import sagar.wellthytest.utils.PermissionUtils.isAccessFineLocationGranted
+import sagar.wellthytest.utils.PermissionUtils.isLocationEnabled
+import sagar.wellthytest.utils.PermissionUtils.requestAccessFineLocationPermission
+import sagar.wellthytest.utils.PermissionUtils.showGPSNotEnabledDialog
+
+class WeatherReportActivity : AppCompatActivity() {
+
+    private var currentLatLng: LatLng? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather_report)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        fetchLocation()
+    }
+
+    private fun fetchLocation(){
+        //if currentLatLng is null check for location permission and get latLng
+        //if currentLatLng is not null then no need to do anything
+        if (currentLatLng == null) {
+            when {
+                this.isAccessFineLocationGranted() -> {
+                    when {
+                        this.isLocationEnabled() -> {
+                            setUpLocationListener()
+                        }
+                        else -> {
+                            this.showGPSNotEnabledDialog()
+                        }
+                    }
+                }
+                else -> {
+                    this.requestAccessFineLocationPermission(LOCATION_PERMISSION_REQUEST_CODE)
+                }
+            }
+        }
+    }
+
+    //function to get last known location if present else get new location
+    private fun setUpLocationListener() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            this.requestAccessFineLocationPermission(LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+            val location = task.result
+            if (location == null){
+                //get new location, if last known location is null
+                getNewLocation()
+            } else {
+                Log.e("getLocationValue", "${location.latitude} - ${location.longitude}")
+            }
+        }
+    }
+
+    //function to get new location
+    private fun getNewLocation(){
+        val locationRequest = LocationRequest.create()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 2
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                val location = p0.lastLocation
+                Log.e("getLocationValueNew", "${location.latitude} - ${location.longitude}")
+            }
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            this.requestAccessFineLocationPermission(LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()!!
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    //setUpLocationListener() //if location permission is given, get current location
+                    fetchLocation()
+                } else {
+                    //display location permission denied msg
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/sagar/wellthytest/utils/PermissionUtils.kt
@@ -0,0 +1,53 @@
+package sagar.wellthytest.utils
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import sagar.wellthytest.R
+
+object PermissionUtils {
+
+    const val LOCATION_PERMISSION_REQUEST_CODE = 201
+
+    //function to request permission from user
+    fun AppCompatActivity.requestAccessFineLocationPermission(requestId: Int) {
+        ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                requestId
+        )
+    }
+
+    //function to check if location permissions are granted or not
+    fun Context.isAccessFineLocationGranted(): Boolean {
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    //function to check if location of the device is enabled or not
+    fun Context.isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    //function to show enable gps dialog
+    fun Context.showGPSNotEnabledDialog() {
+        AlertDialog.Builder(this)
+                .setTitle(this.getString(R.string.enable_gps))
+                .setMessage(this.getString(R.string.required_for_this_app))
+                .setCancelable(false)
+                .setPositiveButton(this.getString(R.string.enable)) { _, _ ->
+                    this.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+                .show()
+    }
+
+}
